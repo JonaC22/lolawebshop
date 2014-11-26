c2dc5d8d85a4a80087f5835c935ab8a1764325fa
Jonathan <jonathan.corallo.utn@gmail.com>
2014-11-26 18:29:17 -0300

    Demo PHP

--- web/index.php
+++ web/index.php
@@ -1,1 +1,15 @@
-<?php include_once("home.html"); ?>
+<?php 
+// Register the Twig templating engine
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path' => __DIR__.'/../views',
+));
+
+$app->get('/twig/{name}', function ($name) use ($app) {
+    return $app['twig']->render('index.twig', array(
+        'name' => $name,
+    ));
+});
+
+//include_once("home.html"); 
+
+?>
