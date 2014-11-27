5c254b6908d318770e0e280df8263e8973ff65d4
Jonathan <jonathan.corallo.utn@gmail.com>
2014-11-27 15:47:39 -0300

    super fix, ahora corre en localhost

--- web/index.php
+++ web/index.php
@@ -1,31 +1,5 @@
 <?php 
-/*require('../vendor/autoload.php');
-$app = new Silex\Application();
-$app['debug'] = true;
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-// Register the Twig templating engine
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-  'twig.path' => __DIR__.'/../views',
-));
-// Our web handlers
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app->render('index.html', ['name' => 'Fabien']);
-  ));
-});
 
-$app->get('/twig/{name}', function($name) use($app) {
-  return $app['twig']->render('index.twig', array(
-    'name' => $name,
-  ));
-});
-
-$app->run();
-*/
-//include lolawebshop home page
 include_once("home.html"); 
 
 ?>
