de704f110845f858a7d391b73ded167199270338
Jonathan <jonathan.corallo.utn@gmail.com>
2014-12-16 12:00:56 -0300

    integracion de postgresql con backend

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
 <?php
-	include_once("home.html"); 
+
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+
+$productos = pg_query($pg_conn, "SELECT * FROM lola.productos");
+
+$categorias = pg_query($pg_conn, "SELECT * FROM lola.categorias");
+
+# Load HTML
+include_once("home.html");
+
 ?>
