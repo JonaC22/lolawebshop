01f5a2d5e816a6144a55c226e6e66cbbb1de351d
Jonathan <jonathan.corallo.utn@gmail.com>
2015-01-06 09:53:19 -0300

    Fixes #1

--- app/views/productos.php
+++ app/views/productos.php
@@ -5,7 +5,8 @@ $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 $result = pg_query($pg_conn, 
 	"SELECT p.id, p.titulo, proveedor, c.titulo categoria, fecha_agregado, imagen 
-	FROM lola.productos p JOIN lola.categorias c on p.categoria = c.id ;");
+	FROM lola.productos p JOIN lola.categorias c on p.categoria = c.id 
+	ORDER BY fecha_agregado DESC;");
 
 $jsonArray = array();
 
