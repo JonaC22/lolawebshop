ed248db880d393f9aa0204e016564430627a42d7
Jonathan <jonathan.corallo92@gmail.com>
2015-02-10 12:30:11 -0300

    fix script & update composer.json

--- auto/prepareScript.sh
+++ auto/prepareScript.sh
@@ -1,5 +1,6 @@
 cp auto/php-fpm.conf vendor/heroku/heroku-buildpack-php/conf/php
 cp auto/heroku.conf.php vendor/heroku/heroku-buildpack-php/conf/nginx
 chmod -R guo+w app/storage
+service apache2 stop
 service nginx stop
 foreman start
